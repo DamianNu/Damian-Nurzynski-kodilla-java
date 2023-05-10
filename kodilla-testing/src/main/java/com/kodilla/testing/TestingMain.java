a9832d185c9b2fbdeb8628a6760a4672c0a6ce5e
DamianNu <damian21v@gmail.com>
2023-05-10 10:37:46 +0200

    6.2 Zadanie: pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-        public static void main(String[] args) {
-            System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
         }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+        int resultAdd = calculator.add(50,50);
+        int resultSubtract = calculator.subtract(101,25);
+
+        if( resultAdd==100 && resultSubtract==76){
+            System.out.println("Calculator - test OK");
+        } else {
+            System.out.println("Calculator - Error");
+        }
+
+    }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a;
+    private int b;
+
+    public int add(int a,int b){
+        int result = a + b;
+       return result;
+    }
+
+    public int subtract(int a, int b){
+        int result = a - b;
+        return result;
+    }
+
+
+}
