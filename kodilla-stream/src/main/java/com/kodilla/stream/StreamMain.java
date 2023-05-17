7d0c3b194b6fea6743ceb5e7c7dae300be5168e4
DamianNu <damian21v@gmail.com>
2023-05-17 21:09:32 +0200

    Zadanie 7.1: upiększacz tekstów

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,23 +1,38 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
+
+
+
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
 
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        String result1 = poemBeautifier.beautify("", a -> "ABC " + a + " ABC");
+        System.out.println(result1);
+
+        String result2 = poemBeautifier.beautify(" kot czy kod ", a -> a.toUpperCase());
+        System.out.println(result2);
+
+        String result3 = poemBeautifier.beautify(" kot czy kod ", a -> "\u001B[34m" + a + "\u001B[0m");
+        System.out.println(result3);
+
+        String result4 = poemBeautifier.beautify(" kot czy kod ", a -> {
+            char[] tab = a.toCharArray();
+            String text = "";
+            int b = 0;
+            for(int i = 0;i< tab.length;i++){
+                text = text + tab[tab.length-1-i];
+            }
+            return text;
+        });
+        System.out.println(result4);
     }
+
 }
