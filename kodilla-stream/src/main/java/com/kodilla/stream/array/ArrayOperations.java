8875163b2a925c4a95f721334e6045728fbb725a
DamianNu <damian21v@gmail.com>
2023-05-25 09:57:12 +0200

    Zadanie 7.5 : obliczamy średnią funkcyjnie (poprawa)

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,12 +4,12 @@ import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
-    static double getAverage(int[] numbers){
-         IntStream.range(0,numbers.length)
+    static double getAverage(int[] numbers) {
+        IntStream.range(0, numbers.length)
                 .map(a -> numbers[a])
                 .forEach(System.out::println);
 
-         OptionalDouble obj = IntStream.range(0,numbers.length)
+        OptionalDouble obj = IntStream.range(0, numbers.length)
                 .map(a -> numbers[a])
                 .average();
         return obj.getAsDouble();
