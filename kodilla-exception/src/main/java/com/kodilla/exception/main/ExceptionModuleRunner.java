5ff91c4f7c3960d6c974d9dc4dc0c994633faad1
DamianNu <damian21v@gmail.com>
2023-05-27 13:28:32 +0200

    Zadanie dodatkowe

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,11 +1,12 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
 
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
-        FileReader fileReader = new FileReader();
-        fileReader.readFile();
+        FileReaderWithoutHandling fileReaderWithoutHandling = new FileReaderWithoutHandling();
+       // fileReaderWithoutHandling.readFile();;
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/Exponentiation.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/Exponentiation.java
@@ -4,20 +4,22 @@ public class Exponentiation {
     public static void main(String[] args) {
         exponentiationAB(0, 1);
         exponentiationAB(3, 0);
-        exponentiationAB(4, 1);
+        exponentiationAB(4, 2);
     }
 
     public static int exponentiationAB(int a, int b) {
-        int result = a;
-        if (a != 0 && b != 0) {
-            for (int i = 1; i < b; i++) {
-                result = result * a;
-            }
-            return result;
+        if (a == 0) {
+            return 0;
         } else if (b == 0) {
             return 1;
         } else {
-            return 0;
+            int result = a;
+            if (a != 0 && b != 0) {
+                for (int i = 1; i < b; i++) {
+                    result = result * a;
+                }
+            }
+            return result;
         }
     }
 }
