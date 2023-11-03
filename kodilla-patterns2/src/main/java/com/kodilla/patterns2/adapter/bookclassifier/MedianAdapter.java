2f918671073a071205686cb38a6ad322b5d2f852
DamianNu <damian21v@gmail.com>
2023-11-03 09:46:50 +0100

    praca nad zadaniem 24.5

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapter.java
@@ -0,0 +1,21 @@
+package com.kodilla.patterns2.adapter.bookclassifier;
+
+import com.kodilla.patterns2.adapter.bookclassifier.librarya.Book;
+import com.kodilla.patterns2.adapter.bookclassifier.librarya.Classifier;
+import com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookSignature;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MedianAdapter extends MedianAdaptee implements Classifier {
+    @Override
+    public int publicationYearMedian(Set<Book> bookSet) {
+        Map<BookSignature, Book> books = new HashMap<>();
+//        for (int i = 0; i < bookSet.size(); i++) {
+//            books.put(new BookSignature(bookSet
+//            )))
+//        }
+        return 0;
+    }
+}
---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.patterns2.adapter.bookclassifier;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedianAdapterTestSuite {
+
+    @Test
+    void publicationYearMedianTest(){
+        // Given
+
+        // When
+
+        // Then
+
+    }
+}
